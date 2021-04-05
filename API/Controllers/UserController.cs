fa9cb47083b270df1c086e3dcc8df585f64b3375
Edier D. Martinez E <eduer11@outlook.com>
2021-04-05 07:41:56 -0500

    V1.0.0

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers(){
+            var users = _context.Users.ToList();
+            return users;
+        }
+    }
+}
