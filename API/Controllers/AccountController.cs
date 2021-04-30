86072632f920572aaf0c03a6c87d3e3fa45f87d4
EdierDavidME <edierdavid000@gmail.com>
2021-04-30 07:34:25 -0500

    V 1.3.2

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,12 +1,14 @@
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController: BaseApiController
+    public class AccountController : BaseApiController
     {
 
         private readonly DataContext _context;
@@ -17,11 +19,15 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password){
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
+        {
+
+            if(await UserExists(registerDTO.Username)) return BadRequest("Mijo o mija, el username ya existe.");
             using var hmac = new HMACSHA512();
-            var user = new AppUser{
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)),
+            var user = new AppUser
+            {
+                UserName = registerDTO.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDTO.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -29,5 +35,11 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
             return user;
         }
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(variable => variable.UserName == username.ToLower());
+        }
+
     }
+
 }
